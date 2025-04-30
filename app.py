4a09678b0f75559b94f04a0f86193f48e73d7963
shivjeetkumar29 <babaofficialarya@gmail.com>
2025-05-01 03:30:16 +0530

    Update app.run configuration to use localhost and disable debug mode

--- app.py
+++ app.py
@@ -23,7 +23,10 @@ def data():
     return jsonify({"message": "Hello from backend!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=False, host='127.0.0.1', port=5000)
+
+
+
 @app.route('/signup', methods=['POST'])
 def signup():
     name = request.form['name']
